d767bc77246f66ce52702b14be5ceaad8a810a17
Florcizmadija <cizmadijaflor@gmail.com>
2025-04-21 23:12:53 -0300

    Se agrega Polimorfismo

--- JavaScript/Proyecto MundoPC/main.js
+++ JavaScript/Proyecto MundoPC/main.js
@@ -15,3 +15,24 @@ const orden = new Orden();
 orden.agregarComputadora(computadora);
 console.log(orden.mostrarOrden());
  
+
+function mostrarInfoDispositivo(dispositivo) {
+    if (dispositivo instanceof Raton) {
+        console.log("Es un Raton:");
+    } else if (dispositivo instanceof Teclado) {
+        console.log("Es un Teclado:");
+    } else if (dispositivo instanceof Monitor) {
+        console.log("Es un Monitor:");
+    } else if (dispositivo instanceof Computadora) {
+        console.log("Es una Computadora:");
+    } else {
+        console.log("Tipo desconocido:");
+    }
+
+    console.log(dispositivo.toString());
+} 
+
+mostrarInfoDispositivo(raton);
+mostrarInfoDispositivo(teclado);
+mostrarInfoDispositivo(monitor);
+mostrarInfoDispositivo(computadora);
