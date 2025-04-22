7f50289e184bd11d2505c972a93de8ca48d7f585
GabrielBruni <166764577+GabrielBruni@users.noreply.github.com>
2025-04-21 23:46:40 -0300

    Update catalogo_peliculas.py

---
+++ Python/Clase03/catalogo_peliculas.py
@@ -0,0 +1,26 @@
+import os
+
+class CatalogoPeliculas:
+    ruta_archivo = "peliculas.txt"
+
+    @staticmethod
+    def agregar_pelicula(pelicula):
+        with open(CatalogoPeliculas.ruta_archivo, 'a', encoding='utf-8') as archivo:
+            archivo.write(str(pelicula) + '\n')
+
+    @staticmethod
+    def listar_peliculas():
+        if os.path.exists(CatalogoPeliculas.ruta_archivo):
+            with open(CatalogoPeliculas.ruta_archivo, 'r', encoding='utf-8') as archivo:
+                print("Películas en catálogo:")
+                print(archivo.read())
+        else:
+            print("El archivo no existe.")
+
+    @staticmethod
+    def eliminar():
+        if os.path.exists(CatalogoPeliculas.ruta_archivo):
+            os.remove(CatalogoPeliculas.ruta_archivo)
+            print("Archivo eliminado.")
+        else:
+            print("El archivo no existe.")
