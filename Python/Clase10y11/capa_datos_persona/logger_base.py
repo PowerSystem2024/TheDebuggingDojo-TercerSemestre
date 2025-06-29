97442ff05ec0e06a3ee4350b7ab4988d5d3c70f2
GonzaJuarez <gonzaloarieljuarez11@gmail.com>
2025-06-29 20:54:16 -0300

    Agregando las Clases 10 y 11 de python

---
+++ Python/Clase10y11/capa_datos_persona/logger_base.py
@@ -0,0 +1,20 @@
+import logging as log
+# log = logging
+
+# https://docs.python.org/es/3.13/howto/logging.html
+
+# Configuración básica
+# log.basicConfig(level=log.DEBUG) # probar con: INFO; WARNING; ERROR; CRITICAL
+log.basicConfig(level=log.DEBUG, format='%(asctime)s: %(levelname)s [%(filename)s: %(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
+
